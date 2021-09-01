dd0b4c326e3b7484e91263d3be5cdbf184a1c5fb
Sai Charan <c-sleakireddy@dovercorp.com>
2021-09-01 09:14:45 +0530

    adding simplilearn assignment

---
+++ SimplilearnAssignment/Retrieve Student Data from a Text File/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Retrieve_Student_Data_from_a_Text_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"D:\Training\SimplilearnAssignment\student_list.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
